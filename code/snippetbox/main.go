4df8c2d33a7dbcd39418d4c3826a02341c8c0cb7
Ak <ak@ak-IdeaPad-Gaming-3-15IMH05.lan>
2025-06-04 15:39:17 +0530

    Correcting the codebase

---
+++ code/snippetbox/main.go
@@ -0,0 +1,26 @@
+package main
+import (
+	"log"
+	"net/http"
+)
+func home(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/" {
+		http.NotFound(w,r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Create a new Snippet..."))
+}
+func main(){
+	mux := http.NewServeMux()
+	mux.HandleFunc("/",home)
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000",mux)
+}
